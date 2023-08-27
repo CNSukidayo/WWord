a7032e7d6a12293dccef87418e9a4f801611ff89
sukidayo <2985384723@qq.com>
2023-08-27 22:43:22 +0800

    添加组校验注解

---
+++ model/model-common/src/main/java/io/github/cnsukidayo/wword/model/validate/CreateCheck.java
@@ -0,0 +1,8 @@
+package io.github.cnsukidayo.wword.model.validate;
+
+/**
+ * @author sukidayo
+ * @date 2023/8/27 19:21
+ */
+public interface CreateCheck {
+}
---
+++ model/model-common/src/main/java/io/github/cnsukidayo/wword/model/validate/DefaultCheck.java
@@ -0,0 +1,8 @@
+package io.github.cnsukidayo.wword.model.validate;
+
+/**
+ * @author sukidayo
+ * @date 2023/8/27 21:36
+ */
+public interface DefaultCheck {
+}
